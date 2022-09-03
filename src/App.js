3153a565f87f80b757021890ef9625ce9fc4d427
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-03 12:49:09 +0500

    Create functions to update score + pass as props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 
 function App() {
+  const { currentScore, setCurrentScore } = useState(0);
+  const { highScore, setHighScore } = useState(0);
+
+  const updateCurrent = (score) => setCurrentScore(score);
+  const updateHigh = (score) => setHighScore(score);
+
   return (
     <div className="App">
-      <NavBar />
-      <Main />
+      <NavBar currentScore={currentScore} highScore={highScore} />
+      <Main
+        currentScore={currentScore}
+        highScore={highScore}
+        updateCurrent={updateCurrent}
+        updateHigh={updateHigh}
+      />
     </div>
   );
 }
