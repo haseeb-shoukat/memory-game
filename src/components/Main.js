2aeecb85bcd6d798a188f987bf7247f31dd9ad46
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-03 15:14:19 +0500

    Remove redundant props

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-const Main = ({ currentScore, highScore, updateCurrent, updateHigh }) => {
+const Main = ({ currentScore, updateCurrent }) => {
   let items = [
     "batman",
     "black-panther",
