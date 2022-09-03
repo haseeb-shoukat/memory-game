c849cb6fda62226ba3592f12ed0e35f02d55a04d
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-03 12:15:06 +0500

    Create Main component
    Initialize items array which contains names of images. Get the shuffle
    function from Stack Overflow. Create the selectedItems state to store
    items which have been picked by the user

---
+++ src/components/Main.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const Main = () => {
+  let items = [
+    "batman",
+    "black-panther",
+    "daredevil",
+    "deadpool",
+    "dredd",
+    "flash",
+    "groot",
+    "hellboy",
+    "hulk",
+    "ironman",
+    "moon-knight",
+    "naruto",
+    "spawn",
+    "spiderman",
+    "vision",
+    "yoda",
+  ];
+
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const shuffle = (arr) => {
+    let currentIndex = arr.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [arr[currentIndex], arr[randomIndex]] = [
+        arr[randomIndex],
+        arr[currentIndex],
+      ];
+    }
+
+    return arr;
+  };
+  return null;
+};
+
+export default Main;
