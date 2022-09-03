58f5192cf5051e4f91c3ab39171f3207bd2187af
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-03 12:02:21 +0500

    Create the NavBar component
    NavBar displays the game's name and shows high score and current
    scores. It does not handle updating the scores, it simply displays them.

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = ({ highScore, currentScore }) => {
+  return (
+    <div class="nav-bar">
+      <div class="heading">Superhero Memory</div>
+      <div class="score">
+        <div class="best-score">High Score: {highScore}</div>
+        <div class="current-score">Current Score: {currentScore}</div>
+      </div>
+    </div>
+  );
+};
